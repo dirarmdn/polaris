0f9e334c15c973d67ad5ac0b5c6132e9ec9deb85
dirarmdn <dhiraramadini@gmail.com>
2024-09-24 12:00:17 +0700

    feat: added templating

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="">
+    <h2><span>Satu Portal </span>untuk Semua Kebutuhan Aplikasi Internal <span>Polban</span></h2>
+</div>
+<div class=""></div>
+@endsection
