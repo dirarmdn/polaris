4531641e9a15e461bd5d28045a861794ab3ceba9
erinadwy <158986567+erinadwy@users.noreply.github.com>
2024-10-20 19:15:01 +0700

    Update web.php
    feat(signup): menambah fungsi register

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PengajuanController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
 
 Route::get('/', [HomeController::class,'index'])->name('home');
 Route::get('/about', [HomeController::class,'about'])->name('home.about');
